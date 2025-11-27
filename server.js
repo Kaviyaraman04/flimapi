4d0dc0cda2ba3304bf1081c1e37d5b4b4c137208
gowrisathya288-arch <gowrisathya288@gmail.com>
2025-11-27 14:29:44 +0530

    chages

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const { Resend } = require("resend");
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
@@ -38,4 +39,4 @@ app.get("/get", (req, res) => {
   res.send("Backend running...");
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(PORT, () => console.log("Server running on port " + PORT));
